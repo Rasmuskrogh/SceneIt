53a7c32745eab62e1c299a9b334060a4c293fc86
[Rasmus Krogh-Andersen] <[rasmus.kroghandersen@gmail.com]>
2024-10-20 22:52:05 +0200

    seting up for integration with strapi backend

--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,3 @@
-export const API = "http://localhost:1337/api";
+export const API = process.env.REACT_APP_HOST || "http://localhost:1337/api";
 export const AUTH_TOKEN = "authToken";
 export const BEARER = "Bearer";
