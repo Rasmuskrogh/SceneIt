bef4c1a5287dd62bb7ba16967cd75e4e0f00e3be
[Rasmus Krogh-Andersen] <[rasmus.kroghandersen@gmail.com]>
2024-10-21 10:20:52 +0200

    changing port locally

--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,3 @@
-export const API = process.env.REACT_APP_HOST || "http://localhost:1337/api";
+export const API = process.env.REACT_APP_HOST || "http://localhost:10000/api";
 export const AUTH_TOKEN = "authToken";
 export const BEARER = "Bearer";
