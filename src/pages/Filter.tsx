8481b06233d1351aba57a3a390913137aac505f5
[Rasmus Krogh-Andersen] <[rasmus.kroghandersen@gmail.com]>
2024-10-22 13:31:51 +0200

    added under construction text for filters page

--- src/pages/Filter.tsx
+++ src/pages/Filter.tsx
@@ -1,7 +1,11 @@
 import "../css/filter.css";
 
 function Filter() {
-  return <div className="wrapper">Filter</div>;
+  return (
+    <div className="wrapper">
+      <h1 className="filters-placeholder">Page under Construction</h1>
+    </div>
+  );
 }
 
 export default Filter;
