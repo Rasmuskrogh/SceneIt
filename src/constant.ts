3191aa8eb4804cfc5864a611f58172f1cc4c2ae4
[Rasmus Krogh-Andersen] <[rasmus.kroghandersen@gmail.com]>
2024-10-23 14:31:20 +0200

    making a special branch for production

--- src/constant.ts
+++ src/constant.ts
@@ -1,6 +1,6 @@
 export const API =
-  /* process.env.REACT_APP_HOST */ //"https://final-react-project-lexicon-strapi.onrender.com/api";
+  /* process.env.REACT_APP_HOST */ "https://final-react-project-lexicon-strapi.onrender.com/api";
 
-  "http://localhost:10000/api";
+/* || "http://localhost:10000/api" */
 export const AUTH_TOKEN = "authToken";
 export const BEARER = "Bearer";
