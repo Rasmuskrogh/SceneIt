2d2f79f002381cd5f22ed12ee8bc5d883faec76f
[Rasmus Krogh-Andersen] <[rasmus.kroghandersen@gmail.com]>
2024-10-23 14:54:12 +0200

    reverting code to local development

--- src/constant.ts
+++ src/constant.ts
@@ -1,6 +1,3 @@
-export const API =
-  /* process.env.REACT_APP_HOST */ //"https://final-react-project-lexicon-strapi.onrender.com/api";
-
-  "http://localhost:10000/api";
+export const API = "http://localhost:1337/api";
 export const AUTH_TOKEN = "authToken";
 export const BEARER = "Bearer";
